82b2396950436467360b3b210267126135bfab5c
AtesIsf <isfendiyar510@gmail.com>
2022-04-20 08:36:17 +0300

    Created Models

--- models/ANNmodel.py
+++ models/ANNmodel.py
@@ -1,1 +1,8 @@
-import keras
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout, Dense, Conv2D, Flatten
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.python.keras.layers.pooling import MaxPool2D
+import numpy as np
+
+ann_model = Sequential()
--- models/SNNmodel.py
+++ models/SNNmodel.py
@@ -1,6 +1,6 @@
 import nengo
+import nengo_dl
+import nengo_extras
+import numpy as np
 
 snn_model = nengo.Network()
-
-with snn_model:
-    pass
