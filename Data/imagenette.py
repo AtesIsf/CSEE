ff37c077209ba3e3485ffbd454a4eef5a0855b98
AtesIsf <isfendiyar510@gmail.com>
2022-08-26 11:21:16 +0300

    Normalized Imagenette

--- Data/imagenette.py
+++ Data/imagenette.py
@@ -26,11 +26,31 @@ imagenette_labels = (
     "Golf Ball",
     "Parachute"
 )
-# TODO: NORMALIZE DATA OR CHANGE DATASET!!!
+
+imagenette_train = []
+imagenette_test = []
+
+for i in tqdm(range(len(imagenette_ds["train"][:]["image"]))):
+    img = imagenette_ds["train"][i]["image"]
+    if img.size[0] == 320: 
+        img = img.rotate(90, expand=1)
+    img = img.crop((0, 0, 426, 320))
+    imagenette_train.append(img)
+
+for i in tqdm(range(len(imagenette_ds["validation"][:]["image"]))):
+    img = imagenette_ds["validation"][i]["image"]
+    if img.size[0] == 320: 
+        img = img.rotate(90, expand=1)
+    img = img.crop((0, 0, 426, 320))
+    imagenette_test.append(img)
+
+
 if __name__ == "__main__":
     # print(imagenette_ds.keys())
-    for i in range(5):
-        plt.imshow(imagenette_ds["train"][i]["image"])
+    for i in range(10):
+        # plt.imshow(imagenette_ds["train"][i]["image"])
+        plt.imshow(imagenette_train[i])
         plt.title(imagenette_labels[imagenette_ds["train"][i]["label"]])
-        print(imagenette_ds["train"][i]["image"].size)
+        print(f"Actual Shape of Image {i+1}: {imagenette_ds['train'][i]['image'].size}")
+        print(f"Normalized Shape of Image {i+1}: {imagenette_train[i].size}")
         plt.show()
