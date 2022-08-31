90ab31996a3bdb3f1aaee91ef69805a46b3eec5f
AtesIsf <isfendiyar510@gmail.com>
2022-08-31 19:49:10 +0300

    Changed the models folder

--- models/ANNmodel.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dropout, Dense, Conv2D, Flatten
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.python.keras.layers.pooling import MaxPool2D
-import numpy as np
-
-ann_model = Sequential()
--- models/SNNmodel.py
+++
@@ -1,6 +0,0 @@
-import nengo
-import nengo_dl
-import nengo_extras
-import numpy as np
-
-snn_model = nengo.Network()
