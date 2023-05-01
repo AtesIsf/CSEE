76653d76332129eec2b1308900efa3284b55a700
Ates <88388753+AtesIsf@users.noreply.github.com>
2023-05-01 23:30:52 +0300

    Changed CIFAR-100 Labels and some links

--- Data/CIFAR100.py
+++ Data/CIFAR100.py
@@ -4,23 +4,14 @@ import matplotlib.pyplot as plt
 cifar_ds = load_dataset("cifar100").shuffle()
 
 cifar_labels = (
-    "Apple", "Aquarium Fish", "Baby", "Bear", "Beaver", "Bed", "Bee",
-    "Beetle", "Bicycle", "Bottle", "Bowl", "Boy", "Bridge", "Bus",
-    "Butterfly", "Camel", "Can", "Castle", "Caterpillar", "Cattle",
-    "Chair", "Chimpanzee", "Clock", "Cloud", "Cockroach", "Couch",
-    "Cra", "Crocodile", "Cup", "Dinosaur", "Dolphin", "Elephant",
-    "Flatfish", "Forest", "Fox", "Girl", "Hamster", "House", "Kangaroo",
-    "Keyboard", "Lamp", "Lawn Mower", "Leopard", "Lion", "Lizard",
-    "Lobster", "Man", "Maple Tree", "Motorcycle", "Mountain", "Mouse",
-    "Mushroom", "Oak Tree", "Orange", "Orchid", "Otter", "Palm_tree", "Pear",
-    "Pickup Truck", "Pine_tree", "Plain", "Plate", "Poppy", "Porcupine", "Possum",
-    "Rabbit", "Raccoon", "Ray", "Road", "Rocket", "Rose", "Sea", "Seal", "Shark",
-    "Shrew", "Skunk", "Skyscraper", "Snail", "Snake", "Spider", "Squirrel", 
-    "Streetcar", "Sunflower", "Sweet Pepper", "Table", "Tank", "Telephone",
-    "Television", "Tiger", "Tractor", "Train", "Trout", "Tulip", "Turtle", 
-    "Tardrobe", "Thale", "Willow Tree", "Wolf", "Woman", "Worm",
+    "Aquatic Mammals", "Fish", "Flowers", "Food Containers", 
+    "Fruit and Vegetables", "Household Electrical Device", 
+    "Household Furniture", "Insects", "Large Carnivores", 
+    "Large Man-made Outdoor Things", "Large Natural Outdoor Scenes", 
+    "Large Omnivores and Herbivores", "Medium-sized Mammals", 
+    "Non-insect Invertebrates", "People", "Reptiles", "Small Mammals", 
+    "Trees", "Vehicles 1", "Vehicles 2"
 )
-
 CIFAR_SHAPE = (32, 32)
 
 # Splits: train, test
@@ -33,5 +24,5 @@ CIFAR_SHAPE = (32, 32)
 if __name__ == "__main__":
     for i in range(10):
         plt.imshow(cifar_ds["train"][i]["img"])
-        plt.title(cifar_labels[cifar_ds["train"][i]["fine_label"]])
+        plt.title(cifar_labels[cifar_ds["train"][i]["coarse_label"]])
         plt.show()
